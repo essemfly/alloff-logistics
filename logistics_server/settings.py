bf674101c105ae061ec48359191a816b6ffec365
ClareKang <shiningkey89@gmail.com>
2022-01-12 14:22:05 +0900

    feat: add configs to setting server instance

--- logistics_server/settings.py
+++ logistics_server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from dotenv import load_dotenv, dotenv_values
 
@@ -143,6 +144,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
