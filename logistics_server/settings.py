c3de28b9836d418babef85c5b8171e29d1790ece
ClareKang <shiningkey89@gmail.com>
2022-01-12 12:09:39 +0900

    feat: add uvicorn settings

--- logistics_server/settings.py
+++ logistics_server/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+]
 
 
 # Application definition
